ed11d852c66bc7fb70620c638a6427222d8842ac
ctdung27 <caotiendung2020@gamil.com>
2022-11-19 04:30:33 +0700

    first commit

---
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  appName: 'Udagram DUTN1',
+  apiHost: 'a330a32b3c8bb4df1a47511abc94474e-1208555369.us-east-2.elb.amazonaws.com/api/v0'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
